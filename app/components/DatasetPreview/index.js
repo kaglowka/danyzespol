0ef77b17a76b5768b2d8f0caeedf83e6fa7f4492
Kasia Skowrońska <kskowronska@bevy.pl>
2018-10-27 23:49:47 +0200

    Add dataset preview

--- app/components/DatasetPreview/index.js
+++ app/components/DatasetPreview/index.js
@@ -8,28 +8,32 @@ import './style.scss';
 export default class DatasetPreview extends React.Component {
 	render() {
 		var gridStyle = {
-			gridTemplateColumns: 'auto auto auto auto',
+			gridTemplateColumns: '150px' + ' auto'.repeat(this.props.columns.length),
 		}
 
 
 		return (
 			<div className="dataset-preview">
 				<div className="preview-table" style={gridStyle}>
+					<div className="row-title">Nagłówek kolumny</div>
 					{this.props.columns.map((column) => (
 						<div className={cnames('column-header', 'cell')}>
 							{column.header}
 						</div>),
 					)}
+					<div className="row-title">Rodzaj danych</div>
 					{this.props.columns.map((column) => (
 						<div className={cnames('column-type', 'cell', column.type)}>
 							{column.type === 'string' ? 'tekst' : 'liczba'}
 						</div>),
 					)}
+					<div className="row-title">Liczba niepowtarzalnych wyników</div>
 					{this.props.columns.map((column) => (
 						<div className={cnames('column-unique', 'cell')}>
 							{column.unique}
 						</div>),
 					)}
+					<div className="row-title">Zakres</div>
 					{this.props.columns.map((column) => (
 						<div className={cnames('column-range', 'cell')}>
 							{column.rangeFrom} - {column.rangeTo}
