e5d701aa37e0d06c8bc00b5c02b24d4757f35e4b
Kasia Skowrońska <kskowronska@bevy.pl>
2018-10-28 01:39:20 +0200

    Chart editor option buttons

--- app/components/ChartEditor/index.js
+++ app/components/ChartEditor/index.js
@@ -6,25 +6,63 @@ import './style.scss';
 
 
 export default class ChartEditor extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			selectedOnX: undefined,
+			selectedOnY: undefined,
+		}
+	}
+
+	handleXOptionClick(option) {
+		this.setState({selectedOnX: option})
+	}
+
+	handleYOptionClick(option) {
+		this.setState({selectedOnY: option})
+	}
 
 	renderYAxisOptions(columns) {
 		return (
-			columns
-				.filter((column) => column.type === 'number')
-				.map((column) => (
-					<div>{column.header}</div>
-				),
-				)
+			<div className="axis-options">
+				<div className={cnames("option-tile", this.state.selectedOnY === "sum" ? 'selected' : '')} onClick={() => this.handleYOptionClick("sum")}>
+					Zlicz wszystkie
+				</div>
+				{columns
+					.filter((column) => column.type === 'number')
+					.map((column) => (
+						<div className={cnames("option-tile", column.header === this.state.selectedOnY ? 'selected' : '')} onClick={() => this.handleYOptionClick(column.header)}>
+							{column.header}
+						</div>
+					),
+					)}
+			</div>
 		)
-
 	}
+
+	renderXAxisOptions(columns) {
+		return (
+			<div className="axis-options">
+				{columns
+					.map((column) => (
+						<div className={cnames("option-tile", column.header === this.state.selectedOnX ? 'selected' : '')} onClick={() => this.handleXOptionClick(column.header)}>
+							{column.header}
+						</div>
+					),
+					)}
+			</div>
+		)
+	}
+
+
 	render() {
 		return (
 			<div className="chart-editor">
 				<div>
 					<h4>Oś pozioma</h4>
 					<p>Służy do wskazania przedziału danych. Na przykład jeśli chcesz zobaczyć jak wybrane przez Ciebie dane rozkładają się w kolejnych latach, wybierz na oś poziomą kolumnę zawierającą daty. Na osi poziomej możesz umieścić zarówno dane z kolumny w formacie tekst jak i liczba.</p>
-
+					{this.props.columns && this.renderXAxisOptions(this.props.columns)}
 				</div>
 				<div>
 					<h4>Oś pionowa</h4>
