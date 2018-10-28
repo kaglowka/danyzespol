6eba7ca593520648efa68b0ac6cb47ef0d3025c3
Kasia Skowrońska <kskowronska@bevy.pl>
2018-10-28 13:06:11 +0100

    Add chart select

--- app/components/Creator/index.js
+++ app/components/Creator/index.js
@@ -4,6 +4,7 @@ import cnames from 'classnames'
 
 import './style.scss';
 
+import ChartSlider from '../ChartSlider';
 import DatasetPreview from '../DatasetPreview';
 import ChartEditor from '../ChartEditor';
 
@@ -66,9 +67,7 @@ export default class Creator extends React.Component {
 							<h2>Co chcesz zobaczyć?</h2>
 							<p>Histogram jest jednym z najbardziej popularnych wykresów statystycznych. Służy on do przedstawienia liczebności obserwacji w zadanych przedziałach.</p>
 						</div>
-						<div className="chart-slider">
-							Różne wykresy
-						</div>
+						<ChartSlider />
 					</div>
 				</section>
 				<section className="data-section">
